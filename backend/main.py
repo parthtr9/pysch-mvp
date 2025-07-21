ebcfabe845a1a71c428f715d5505de475be01c98
Parth Trived <parthtttrivedi@gmail.com>
2025-07-21 13:09:43 +0530

    feat: restructure as monorepo and add backend

---
+++ backend/main.py
@@ -0,0 +1,26 @@
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import json
+
+app = FastAPI()
+
+# Configure CORS
+origins = [
+    "https://parthtr9.github.io",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api/topics")
+def get_topics():
+    with open("db.json") as f:
+        data = json.load(f)
+    return data
